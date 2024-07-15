94cc3a2df1cd39602d55c6df19f58a8b82c97c56
DouyonDev <abdoulayedouyon.ad@gmail.com>
2024-07-15 16:20:21 +0000

    implementation des services fideliter, personnel et contrat

--- penkuru/src/main/java/odk/g1/penkuru/penkuru/Repository/PersonnelRepository.java
+++ penkuru/src/main/java/odk/g1/penkuru/penkuru/Repository/PersonnelRepository.java
@@ -3,7 +3,9 @@ package odk.g1.penkuru.penkuru.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import odk.g1.penkuru.penkuru.Models.Personnel;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonnelRepository extends JpaRepository<Personnel, Long>{
-    
+
 }
