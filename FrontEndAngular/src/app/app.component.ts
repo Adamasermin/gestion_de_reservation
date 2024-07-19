85a41b6df844a711994544a19bbdf2d666a5d379
Adamasermin <serminadama2@gmail.com>
2024-07-19 17:36:13 +0000

    bar de recherche

--- FrontEndAngular/src/app/app.component.ts
+++ FrontEndAngular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarNavigationComponent } from "./components/child-components/bar-navigation/bar-navigation.component";
+import { BarRechercheComponent } from "./components/child-components/bar-recherche/bar-recherche.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BarNavigationComponent],
+  imports: [RouterOutlet, BarNavigationComponent, BarRechercheComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
