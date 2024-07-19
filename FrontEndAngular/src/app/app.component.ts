0ec3272f44cdc2178d0787df27253eb9199e5853
Adamasermin <serminadama2@gmail.com>
2024-07-19 14:33:00 +0000

    Bar de navigation

--- FrontEndAngular/src/app/app.component.ts
+++ FrontEndAngular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarNavigationComponent } from "./components/child-components/bar-navigation/bar-navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FrontEndAngular';
+  
 }
